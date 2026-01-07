24e1e3201dc3779187541ff8ef0f9924f9d510a8
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2026-01-07 00:56:32 -0600

    Add get+store funcs for the client
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ internal/client/get.go
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/carabiner-dev/burnafter/internal/common"
+)
+
+// Get retrieves a secret from the server
+func (c *Client) Get(name string) (string, error) {
+	if c.client == nil {
+		return "", fmt.Errorf("not connected to server")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.client.Get(ctx, &pb.GetRequest{
+		Name:        name,
+		ClientNonce: c.options.Nonce,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("getting secret: %w", err)
+	}
+
+	if !resp.Success {
+		return "", fmt.Errorf("server error: %s", resp.Error)
+	}
+
+	return resp.Secret, nil
+}
---
+++ internal/client/store.go
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/carabiner-dev/burnafter/internal/common"
+)
+
+// Store stores a secret on the server
+func (c *Client) Store(name, secret string, ttlSeconds, absoluteExpirationSeconds int64) error {
+	if c.client == nil {
+		return fmt.Errorf("not connected to server")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.client.Store(ctx, &pb.StoreRequest{
+		Name:                      name,
+		Secret:                    secret,
+		TtlSeconds:                ttlSeconds,
+		ClientNonce:               c.options.Nonce,
+		AbsoluteExpirationSeconds: absoluteExpirationSeconds,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to store secret: %w", err)
+	}
+
+	if !resp.Success {
+		return fmt.Errorf("server error: %s", resp.Error)
+	}
+
+	return nil
+}
