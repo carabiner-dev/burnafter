5401384ea98f8bea8670cca8230e196a2a461c1f
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2026-01-06 23:18:58 -0600

    Add options set for client and server
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ internal/options/options.go
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package options
+
+import "time"
+
+// Common options for client and server options
+type Common struct {
+	SocketPath        string
+	DefaultTTL        time.Duration
+	InactivityTimeout time.Duration
+	Debug             bool
+}
+
+// Server options set
+type Server struct {
+	Common
+}
+
+// Client options set
+type Client struct {
+	Common
+}
+
+// defaultCommon default common options shared by default server and client sets
+var defaultCommon = Common{
+	SocketPath:        "/tmp/burnafter.sock",
+	DefaultTTL:        4 * time.Hour,
+	InactivityTimeout: 10 * time.Minute,
+	Debug:             false,
+}
+
+// DefaultClient default client options
+var DefaultClient = &Client{
+	Common: defaultCommon,
+}
+
+// DefaultServer default server options
+var DefaultServer = &Server{
+	Common: defaultCommon,
+}
